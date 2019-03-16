5f36dc74ca59c5216237f740e8fa1e989c81f915
PanaromicHCL <Panaromic@hcl1>
2019-03-15 18:58:28 -0700

    Final Commit

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var APP_BASE_URL = "http://10.117.189.212:8081/Insurance/api";
+
+// http://10.117.189.72:8081/Insurance/api/policydetails
+
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
   'ngRoute',
--- app/policylist/policylist.js
+++ app/policylist/policylist.js
@@ -1,47 +1,47 @@
 'use strict';
 
-var policiesList = [
-    {
-        name: "Jicko",
-        entryage: "12",
-        maxmatage: "N/A",
-        term: "N/A",
-        minprem: "Rs. 100000",
-        minsumass: "Rs. 7190/- Annually"
-    },
-    {
-        name: "Jicko",
-        entryage: "12",
-        maxmatage: "N/A",
-        term: "N/A",
-        minprem: "Rs. 100000",
-        minsumass: "Rs. 7190/- Annually"
-    },
-    {
-        name: "Jicko",
-        entryage: "12",
-        maxmatage: "N/A",
-        term: "N/A",
-        minprem: "Rs. 100000",
-        minsumass: "Rs. 7190/- Annually"
-    },
-    {
-        name: "Jicko",
-        entryage: "12",
-        maxmatage: "N/A",
-        term: "N/A",
-        minprem: "Rs. 100000",
-        minsumass: "Rs. 7190/- Annually"
-    },
-    {
-        name: "Jicko",
-        entryage: "12",
-        maxmatage: "N/A",
-        term: "N/A",
-        minprem: "Rs. 100000",
-        minsumass: "Rs. 7190/- Annually"
-    }
-];
+// var policiesList = [
+//     {
+//         name: "Jicko",
+//         entryage: "12",
+//         maxmatage: "N/A",
+//         term: "N/A",
+//         minprem: "Rs. 100000",
+//         minsumass: "Rs. 7190/- Annually"
+//     },
+//     {
+//         name: "Jicko",
+//         entryage: "12",
+//         maxmatage: "N/A",
+//         term: "N/A",
+//         minprem: "Rs. 100000",
+//         minsumass: "Rs. 7190/- Annually"
+//     },
+//     {
+//         name: "Jicko",
+//         entryage: "12",
+//         maxmatage: "N/A",
+//         term: "N/A",
+//         minprem: "Rs. 100000",
+//         minsumass: "Rs. 7190/- Annually"
+//     },
+//     {
+//         name: "Jicko",
+//         entryage: "12",
+//         maxmatage: "N/A",
+//         term: "N/A",
+//         minprem: "Rs. 100000",
+//         minsumass: "Rs. 7190/- Annually"
+//     },
+//     {
+//         name: "Jicko",
+//         entryage: "12",
+//         maxmatage: "N/A",
+//         term: "N/A",
+//         minprem: "Rs. 100000",
+//         minsumass: "Rs. 7190/- Annually"
+//     }
+// ];
 
 angular.module('myApp.policylist', ['ngRoute'])
 
@@ -54,10 +54,26 @@ angular.module('myApp.policylist', ['ngRoute'])
 
 .controller('PoliciesListCtrl', function($scope, $http) {
 
-    $scope.polList = policiesList;
+    // $scope.polList = policiesList;
+    // $scope.terms = "";
 
-    // $http.get(APP_BASE_URL + "/pets")
-    // .then(function(response) {
-    //     $scope.petsList = response.data;
-    // });
+    $http.get(APP_BASE_URL + "/policies")
+    .then(function(response) {
+        //console.log("Data:" + response);
+        $scope.polList = response.data;
+    });
+
+    $scope.showData = function(policyId) {
+        // console.log("Data:" + policyId);
+
+        $http.get("http://10.117.189.72:8081/Insurance/api/policydetail/" + policyId)
+        .then(function(response) {
+            // console.log(JSON.stringify(response.data.textualDescription));
+            $scope.polDetails = response.data;
+        });
+    }
+
+    $scope.buy = function() {
+        alert("Thanks for buying a policy !!!");
+    }
 });
